35e196a7cfd59b9b76aa6d873e4171474fa37a1f
Bello Abraham <belloabraham101@gmail.com>
2022-05-10 21:52:45 +0100

    Added css files

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-import './main.scss';
+import "./main.scss";
+import AOS from "aos";
+
+
+AOS.init();
+
+//Mobile nav menu toggle
+const toggler = document.querySelector(".menu__toggler");
+const menu = document.querySelector(".menu");
+
+/*
+ * Toggles on and off the 'active' class on the menu
+ * and the toggler button.
+ */
+toggler.addEventListener("click", () => {
+  toggler.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+
+//Google Analytics
+function enableTracking() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+
+  gtag("config", "G-5LXKYHJ4Y4");
+}
+
+ enableTracking();
+

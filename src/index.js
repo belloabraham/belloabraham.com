c529748299cc1e6c821e31b30ed9b3d7af7999d1
Bello Abraham <belloabraham101@gmail.com>
2022-05-25 09:05:27 +0100

    Added preloader

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './index.scss';
 import './mobile-menu';
+import $ from 'jquery'
+
+window.addEventListener("load", function(){
+    hidePreloader();
+}) 
+
+
+function hidePreloader() {
+    const preloader = $('.preloader')
+    preloader.hide();
+}
